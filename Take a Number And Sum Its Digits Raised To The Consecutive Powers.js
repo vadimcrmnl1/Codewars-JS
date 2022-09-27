1c2e867678a13297dcc31c6394221f2d202f370d
Vadim Karpenko <vadzimkarpenka@gmail.com>
2022-09-27 16:24:08 +0300

    Codewars JS

--- Take a Number And Sum Its Digits Raised To The Consecutive Powers.js
+++ Take a Number And Sum Its Digits Raised To The Consecutive Powers.js
@@ -2,7 +2,7 @@ function sumDigPow(a, b) {
     eureka = [];
     for(i=a; i <=b; i++){
         digits = String(i).split('');
-        if(i == digits.reduce(function(accumulator, currentValue, currentIndex){
+        if(i === digits.reduce(function(accumulator, currentValue, currentIndex){
             return accumulator + currentValue**(currentIndex+1);
         }, 0)){
             eureka.push(i);
